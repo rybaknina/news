85fb5dc6604a88cb5b10657d362ab0b741d24202
Nina Rybak <nrybak@dev.vtb.ru>
2023-04-02 15:38:39 +0300

    task-8

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = \Auth::user();
+
+        if ($user->is_admin === false) {
+            \abort(404);
+        }
+        return $next($request);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\Admin\IndexController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+use App\Http\Controllers\Account\IndexController as AccountController;
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('/', IndexController::class)
-    ->name('index');
-    Route::resource('categories', AdminCategoryController::class);
-    Route::resource('news', AdminNewsController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/account', AccountController::class)
+        ->name('account');
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
+        Route::get('/', IndexController::class)
+            ->name('index');
+        Route::resource('categories', AdminCategoryController::class);
+        Route::resource('news', AdminNewsController::class);
+    });
 });
